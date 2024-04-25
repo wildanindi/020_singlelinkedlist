750feca9e777cc102816d52252ea2f0b7b0828e3
wildanindi <wildanindi2@gmail.com>
2024-04-25 17:16:05 +0700

    menggunakan fungsi if untuk melakukan perbandingan pada data noMhs dan nim

--- single_list.cpp
+++ single_list.cpp
@@ -58,4 +58,17 @@ bool searchNode(int nim, Node* current, Node* previous){
         previous = current;
         current = current->next;
     }
+
+    if (current == NULL)
+    {
+        return false;
+    }
+    else if (current->noMhs == nim)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
