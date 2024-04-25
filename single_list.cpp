1e7c432b302f960141b7b6ed0f86f7e0d68f0653
wildanindi <wildanindi2@gmail.com>
2024-04-25 17:22:27 +0700

    menambah fungsi if untuk melakukan perbandingan pada nim yang di cari

--- single_list.cpp
+++ single_list.cpp
@@ -75,5 +75,10 @@ bool searchNode(int nim, Node* current, Node* previous){
 bool deleteNode(int nim){
     Node* current = START;
     Node* previous = START;
-       
+    if (searchNode(nim, previous, current) == false)
+        return false;
+    previous->next = current->next;
+    if (current == START)
+        START = current->next;
+    return true;        
 }
