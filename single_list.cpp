9cb58a2addcc65923f1ef2c4b5f2aaa37e2783d9
wildanindi <wildanindi2@gmail.com>
2024-04-25 17:47:25 +0700

    pada fungsi main ini, kita menambahkan case agar pilihan yang kita buat itu bisa di akses oleh pengguna

--- single_list.cpp
+++ single_list.cpp
@@ -133,7 +133,62 @@ int main(){
     int pilihan;
     do
     {
+        try
+        {
+            cout << "1. Tambah Data" << endl;
+            cout << "2. Hapus Data" << endl;
+            cout << "3. Tampilkan Data" << endl;
+            cout << "4. Cari Data" << endl;
+            cout << "5. Keluar" << endl;
+            cout << "Pilihan";
+            cin >> pilihan;
+            switch (pilihan)
+            {
+                case 1:
+                    addNode();
+                    cout << "Data Berhasil Ditambahkan" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                case 2:
+                    if (listEmpty())
+                    {
+                        cout << "List Kosong" << endl;
+                        system("pause");
+                        system("cls");
+                        break;
+                    }    
+
+                    int nim;
+                    cout << "Masukkan NIM: ";
+                    cin >> nim;
+                    if (deleteNode(nim)) {
+                        cout << "nim: " << nim << "Berhasil Dihapus" << endl;
+                        system("pause");
+                        system("cls");
+                    }
+                    else
+                        cout << "Data tidak ditemukan" << endl;
+                    break;
+                case 3:
+                    traverse();
+                    break;
+                case 4:
+                    searchData();
+                    break;
+                case 5:
+                    break;
+                default:
+                    cout << "Pilihan tidak ada" << endl;
+                    break;                    
+            }
+        }
+        catch(exception e)
+        {
+            cout << "Terjadi Kesalahan" << endl;
+        }
+        
         /* code */
-    } while (/* condition */);
+    } while (pilihan != 5);
     
 }
