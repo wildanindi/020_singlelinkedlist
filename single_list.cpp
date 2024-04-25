5ee850b74e51725c51268aebecefcf97f0467eeb
wildanindi <wildanindi2@gmail.com>
2024-04-25 17:23:44 +0700

    menambah boolean dan if untuk mencari list yang kosong

--- single_list.cpp
+++ single_list.cpp
@@ -82,3 +82,9 @@ bool deleteNode(int nim){
         START = current->next;
     return true;        
 }
+bool listEmpty(){
+    if (START == NULL)
+        return true;
+    else
+        return false;    
+}
