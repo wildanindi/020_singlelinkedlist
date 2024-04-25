ec183dbd97e3f096e18b1abea794feed02951026
wildanindi <wildanindi2@gmail.com>
2024-04-25 17:06:37 +0700

    menambahkan while untuk perulangan pada if

--- single_list.cpp
+++ single_list.cpp
@@ -31,4 +31,12 @@ void addNode () {
         START = nodeBaru;
         return;
     }
+
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        
+    }
 }
