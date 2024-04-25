6984f11711ada5da663d35e3599ac71469072a36
wildanindi <wildanindi2@gmail.com>
2024-04-25 17:04:18 +0700

    menambahkan fungsi if untuk mengetahui perbandingan antara nimnya sudah ada atau belum

--- single_list.cpp
+++ single_list.cpp
@@ -19,4 +19,16 @@ void addNode () {
     cin >> nama;
     nodeBaru->noMhs = nim;
     nodeBaru->name = nama;
+
+    if (START == NULL || nim <= START->noMhs){
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
 }
