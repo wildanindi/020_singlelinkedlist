4695eef1f97f592911d54b271fc00b8e08136de6
wildanindi <wildanindi2@gmail.com>
2024-04-25 17:08:56 +0700

    menambah if untuk mengetahui nim dan menambah prosedut cout untuk menampilkan pesan

--- single_list.cpp
+++ single_list.cpp
@@ -37,6 +37,12 @@ void addNode () {
 
     while ((current != NULL) && (nim >= current->noMhs))
     {
-        
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
     }
 }
